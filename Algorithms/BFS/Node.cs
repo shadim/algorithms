41a0136fb627cfc959e5b96753e5f5f822b8f41c
shadim <shadi@shadi-WKS.aternity.com>
2013-08-22 14:34:48 +0300

    add BFS algorithim

---
+++ Algorithms/BFS/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BFS
+{
+    public class Node<T>
+    {
+
+        private IList<Node<T>> childs = new List<Node<T>>();
+
+        public T Data { get; set; }
+
+        public bool Visited { get; set; }
+
+        public IList<Node<T>> Childs
+        {
+            get
+            {
+                return childs;
+            }
+        }
+    }
+}
---
+++ Algorithms/BFS/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node<int> root = new Node<int>();
+            root.Data = 1;
+            root.Childs.Add(new Node<int>() { Data = 2 });
+            root.Childs.Add(new Node<int>() { Data = 3 });
+
+
+
+            List<Node<int>> queue = new List<Node<int>>();
+            queue.Add(root);
+            Console.WriteLine(root.Data);
+            root.Visited = true;
+
+            while (queue.Count > 0)
+            {
+              
+                Node<int> node = queue[0];
+                queue.Remove(node);
+                Node<int> child = null;
+
+                while ((child = GetUnvisitedChildNode(node)) != null)
+                {
+                    child.Visited = true;
+                    Console.WriteLine(child.Data);
+                    queue.Add(child);
+                }
+            }
+        }
+
+        private static Node<int> GetUnvisitedChildNode(Node<int> node)
+        {
+            foreach (Node<int> n in node.Childs)
+                if (n.Visited == false)
+                    return n;
+            
+            return null;
+        }
+    }
+}
