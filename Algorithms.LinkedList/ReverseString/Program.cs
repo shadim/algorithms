e54822015311c646b1885070eed3212ecd46b369
shadim <shadi@shadi-WKS.aternity.com>
2013-08-22 12:12:33 +0300

    add reverse string algorithm

---
+++ Algorithms.LinkedList/ReverseString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] realstring = "123456789abcd".ToCharArray();
+            
+            int len =  realstring.Length;
+
+            Console.WriteLine("Original String:{0}",new String(realstring));
+
+            for (int i = 0; i < len/2; i++)
+            {
+                char lowerchar = realstring[i];
+
+                realstring[i] = realstring[len - i - 1];
+
+                realstring[len - i-1] = lowerchar;
+            }
+
+            Console.WriteLine("Reverse String:{0}",new String(realstring));
+
+            Console.ReadKey();
+        }
+    }
+}
